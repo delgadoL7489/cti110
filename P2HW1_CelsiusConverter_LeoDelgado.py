19edd9a8336b91a800d996726fabc6eaf86f412d
delgadoL7489 <43153782+delgadoL7489@users.noreply.github.com>
2018-09-19 15:52:00 -0400

    Add files via upload

---
+++ P2HW1_CelsiusConverter_LeoDelgado.py
@@ -0,0 +1,20 @@
+#Make a celsius fahrenheit converter
+#09/10/18
+#CTI-110 P2HW1-Celsius Fahrenheit Converter
+#Your Name
+#
+
+#Entering a celsius number to be converted
+Celsius = float(input('Enter a temperature in Celsius: '))
+
+#Calculate the number for fehrenheit
+Fahrenheit = (9/5)* Celsius + 32
+
+#Display the temperature in Fahrenheit
+print('The temperature in Fahrenheit is:',Fahrenheit,'F')
+
+#prompt the use for celsius value
+#save value
+#Imput formula to convert to fahrenheit
+#save value
+#print value in fahrenheit
---
+++ P2HW2_MaleFemale_Percentage_LeoDelgado.py
@@ -0,0 +1,33 @@
+#Calculating Male and Female Percentge
+#09/10/18
+#CTI-110 P2HW2-Nale Female Percentage
+#Leonardo Delgado
+#
+
+#Asks for the amount of males in class
+Male = float(input('The number of males in class: '))
+
+#Asks for the amount of females in class
+Female = float(input('The number of females in class: '))
+
+#Calculate male percentage
+PM=(Male/(Male + Female))
+
+#Calculate female percentage
+PF=(Female/(Male + Female))
+
+#Display percentage of males
+print('The percentage of males in class:',format(PM, '.00%'))
+
+#Display percentage of females
+print('The percentage of females in class:',format(PF, '.00%'))
+
+#prompt the user for the amount of males in class
+#Save value
+#Prompt the user for the amount of females in class
+#save value
+#Input formula for male percentage
+#Save value
+#Input formula for female percentage
+#Save value
+#Display value in percentage of male and females in class
